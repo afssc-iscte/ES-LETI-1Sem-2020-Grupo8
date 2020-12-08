8458bbb46ffb69fe2ec902b051078556ac138b66
afssc-iscte <afssc@iscte-iul.pt>
2020-12-08 12:21:05 +0000

    Valores da tabela alterados

--- simple/src/main/java/projetoES/simple/GUI.java
+++ simple/src/main/java/projetoES/simple/GUI.java
@@ -1,47 +1,26 @@
 package projetoES.simple;
 
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.*;
-import javax.swing.*;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-<<<<<<< HEAD
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.*;
 import java.awt.Font;
-import javax.swing.JComboBox;
 
 public class GUI {
 	private App app;	
 	private JFrame frame;
 	private JButton btnFile;
 	private JButton btnInsert;
-	private JButton btnClear;
-<<<<<<< HEAD
 	private JButton btnSearch;
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
 	private JTextField textField1;
 	private JTextField textField2;
 	private JComboBox<Object> comboBox;
 	private JComboBox<Object> comboBox1;
-	private JComboBox<Object> comboBox2;
-	private String[] metricas = { "LOC", "CYCLO", "ATFD", "LAA"};
-	private String[] metricas2 = {">", "<", "="};
-	private String[] andor = { "AND" , "OR" };
 	private String selected;
 	private String selected1;
 	private String selected2;
-	private String selected3;
 	private JTextPane textPane ;
 	private JScrollPane scrollPane;
 	private JTable excel;
@@ -59,11 +38,6 @@ public class GUI {
 		});
 	}
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
 	public GUI() throws InvalidFormatException, IOException {
 		initialize();
 	}
@@ -71,6 +45,26 @@ public class GUI {
 	private void initialize() throws InvalidFormatException, IOException {
 		initComponents();
 		createEvents();
+	}
+	
+	private void update() {
+		frame.remove(scrollPane);
+		String[][] rowData = new String[app.rowNum()][app.columnNum()];
+		String[] columnNames = new String[app.columnNum()];
+		for (int row=0 ; row<app.rowNum()+1 ; row++) {
+			for (int column=0 ; column<app.columnNum(); column++) {
+				String cell = app.get(row, column);
+				if (row == 0)
+					columnNames[column] = cell;
+				else
+					rowData[row - 1][column] = cell;
+			}
+		}
+		excel = new JTable (rowData, columnNames);
+		excel.setFillsViewportHeight(true);
+		scrollPane = new JScrollPane(excel);
+		scrollPane.setBounds(40, 200, 500, 200 );
+		frame.add(scrollPane);
 	}
 
 	private void initComponents() {
@@ -90,11 +84,13 @@ public class GUI {
 		frame.getContentPane().add(textField1);
 		textField1.setColumns(10);
 
+		String[] metricas = { "LOC", "CYCLO", "ATFD", "LAA"};
 		comboBox = new JComboBox<Object>(metricas);
 		comboBox.setFont(new Font("Century Gothic", Font.PLAIN, 10));
 		comboBox.setBounds(52, 70, 59, 21);
 		frame.getContentPane().add(comboBox);
 
+		String[] metricas2 = {">", "<", "="};
 		comboBox1 = new JComboBox<Object>(metricas2);
 		comboBox1.setFont(new Font("Century Gothic", Font.PLAIN, 10));
 		comboBox1.setBounds(118, 70, 59, 21);
@@ -106,33 +102,16 @@ public class GUI {
 		frame.getContentPane().add(textField2);
 		textField2.setColumns(10);
 
-		comboBox2 = new JComboBox<Object>(andor);
-		comboBox2.setFont(new Font("Century Gothic", Font.PLAIN, 10));
-		comboBox2.setBounds(293, 70, 56, 21);
-<<<<<<< HEAD
-		comboBox2.setEditable(true);
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
-		frame.getContentPane().add(comboBox2);
-
 		btnInsert = new JButton("Insert");
 		btnInsert.setFont(new Font("Century Gothic", Font.PLAIN, 10));
 		btnInsert.setBounds(412, 60, 85, 32);
 		frame.getContentPane().add(btnInsert);
 
-		btnClear = new JButton("Clear");
-		btnClear.setFont(new Font("Century Gothic", Font.PLAIN, 10));
-		btnClear.setBounds(412, 95, 85, 32);
-		frame.getContentPane().add(btnClear);
-
-<<<<<<< HEAD
 		btnSearch = new JButton("Search");
 		btnSearch.setFont(new Font("Century Gothic", Font.PLAIN, 10));
-		btnSearch.setBounds(412, 130, 85, 32);
+		btnSearch.setBounds(412, 95, 85, 32);
 		frame.getContentPane().add(btnSearch);
 
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
 		textPane = new JTextPane();
 		textPane.setFont(new Font("Century Gothic", Font.PLAIN, 10));
 		textPane.setBounds(52, 101, 304, 26);
@@ -140,10 +119,7 @@ public class GUI {
 
 	}
 
-<<<<<<< HEAD
 
-=======
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
 	private void createEvents() throws InvalidFormatException, IOException {
 		btnFile.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		btnFile.addActionListener(new ActionListener() {
@@ -169,17 +145,10 @@ public class GUI {
 							}
 						}
 						excel = new JTable (rowData, columnNames);
-<<<<<<< HEAD
 						excel.setFillsViewportHeight(true);
 						scrollPane = new JScrollPane(excel);
 						scrollPane.setBounds(40, 200, 500, 200 );
 						frame.add(scrollPane);
-=======
-		        		excel.setFillsViewportHeight(true);
-						scrollPane = new JScrollPane(excel);
-		                scrollPane.setBounds(40, 200, 500, 200 );
-		                frame.add(scrollPane);
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
 					} catch (InvalidFormatException | IOException e1) {
 						System.out.println("Something went wrong...");
 					}
@@ -188,19 +157,17 @@ public class GUI {
 
 		btnInsert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textPane.setText("");
 				selected = comboBox.getSelectedItem().toString();
 				selected1 = comboBox1.getSelectedItem().toString();
 				selected2 = textField2.getText();
-				selected3 = comboBox2.getSelectedItem().toString();
 				if (selected2.isEmpty()) {
 					System.out.println("Espaço não preenchido");
 				} else {
 					textPane.setText(textPane.getText()+" "+selected);
 					textPane.setText(textPane.getText()+" "+selected1);
 					textPane.setText(textPane.getText()+" "+selected2);
-					textPane.setText(textPane.getText()+" "+selected3);
-				}		
-<<<<<<< HEAD
+				}
 
 			}	
 		});
@@ -209,131 +176,25 @@ public class GUI {
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String metrics= textPane.getText();
-				String [] v= metrics.split(" ");
-				String cond= String.join("", v[1], v[2]);
-				char c1= cond.charAt(1);
-				char c2= cond.charAt(2);
-				double d2= c2;
-				String c= v[0];
-				String verdadeiro= "VERDADEIRO";
-				String falso= "FALSO";
-
-				try {
-					app.readFile(textField1.getText());
-					String[][] rowData = new String[app.rowNum()][app.columnNum()];
-					String[] columnNames = new String[app.columnNum()];
-					for (int row=0 ; row<app.rowNum()+1 ; row++) {
-						for (int column=0 ; column<app.columnNum(); column++) {
-							String cell = app.get(row, column);
-							if (row == 0)
-								columnNames[column] = cell;
-							else
-								rowData[row - 1][column] = cell;
-						}
-					}
-
-
-					int size= columnNames.length;
-
-					for (int nrColumn=0; nrColumn<size; nrColumn++){
-						if(columnNames[nrColumn]==v[0]) {
-							for (Row row : sheet) { 
-								Cell cell = row.getCell(nrColumn);
-								int index= cell.getRowIndex();
-								double value= cell.getNumericCellValue();
-
-								switch(c1) {
-								case '<':
-									if(value < d2) {
-										cell.escreveVF(c,verdadeiro, index);
-									}
-									else {
-										cell.escreveVF(c,falso, index);
-									}
-								case '>':
-									if(value > d2) {
-										cell.escreveVF(c, verdadeiro, index);
-									}
-									else {
-										cell.escreveVF(c, falso, index);
-									}
-								case '=':
-									if(value == d2) {
-										cell.escreveVF(c, verdadeiro, index);
-									}
-									else {
-										cell.escreveVF(c, falso, index);
-									}
+				if (metrics.isEmpty()) {
+					System.out.println("Nada a procurar!");
+				} else {
+					String[] v= metrics.split(" ");
+					String c = v[1];
+					try {
+						System.out.println("Changing: " + c);
+						for (int i=0; i<app.getColumnNames().length; i++){
+							if(c.equals(app.get(0, i))) {
+								for (int j=1 ; j<app.getRowData().length + 1 ; j++) {
+									Metric m = new Metric(v[1], v[2], Integer.parseInt(v[3]));
+									app.escreveVF(m, j);
 								}
 							}
-
-						}	}
-				});
-			}
-
-			public void  escreveVF (String string, String VF, int ind){
-				app.readFile(textField1.getText());
-				String[][] rowData1 = new String[app.rowNum()][app.columnNum()];
-				String[] columnNames1 = new String[app.columnNum()];
-				for (int row=0 ; row<app.rowNum()+1 ; row++) {
-					for (int column=0 ; column<app.columnNum(); column++) {
-						String cell = app.get(row, column);
-						if (row == 0)
-							columnNames1[column] = cell;
-						else
-							rowData1[row - 1][column] = cell;
-					}
-				}
-				if(string =="LOC" || string =="CYCLO") {
-					for (int nrColumn=0; nrColumn<size; nrColumn++){
-						if(columnNames1[nrColumn]== "is_long_method") {
-							Row r = sheet.getRow(ind); 
-							if (r == null) {
-								r = sheet.createRow(ind);
-							}
-
-							Cell c = r.getCell(nrColumn); 
-							if (c == null) {
-								c = r.createCell(nrColumn, Cell.CELL_TYPE_STRING);
-							}
-							c.setCellValue(VF);
+							update();
 						}
+					} catch(Exception a) {
+						a.printStackTrace();
 					}
-				}
-				if(string =="ATFD" || string =="LAA") {
-					for (int nrColumn=0; nrColumn<size; nrColumn++){
-						if(columnNames1[nrColumn]== "is_feature_envy") {
-							Row r = sheet.getRow(ind); 
-							if (r == null) {
-								r = sheet.createRow(ind);
-							}
-
-							Cell c = r.getCell(nrColumn); 
-							if (c == null) {
-								c = r.createCell(nrColumn, Cell.setCellType(String));
-							}
-							c.setCellValue(VF);
-						}
-					}
-				}
-			}
-
-
-			btnClear.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					textPane.setText("");
-				}
-			}));
-		}
-=======
-			}	
-		});
-
-		btnClear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				textPane.setText("");
-			}
-		});
+				}}});
 	}
 }
->>>>>>> f8e7e59e064ae0e2565e1c63b771c39b41968c95
